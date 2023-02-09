53e1346c591e9fc49c2945ef23b2d4a3b76a8580
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2023-02-09 18:21:10 +0000

    create navigation

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,10 @@ def index(request):
     """Return index.html page"""
 
     return render(request, 'home/index.html')
+
+
+def signup(request):
+
+    """Return signup.html page"""
+
+    return render(request, 'home/signup.html')
