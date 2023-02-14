4762803edc4d101f73d934fa0527e4ebd1e6aec4
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2023-02-14 21:53:08 +0000

    Make users complete the signup at login if they haven't completed it yet.

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserTypeForm
+from .models import UserProfile
 
 
 def index(request):
@@ -28,8 +29,11 @@ def advisor_seeker(request):
             form.instance.user = request.user
             form.save()
 
+            # if Advisor return advisor signup. If seeker return seeker signup.
+
             if form.instance.user_type == 0:
                 return redirect('advisor_signup')
+
             elif form.instance.user_type == 1:
                 return redirect('seeker_signup')
 
@@ -41,4 +45,22 @@ def advisor_seeker(request):
         'form': form,
         'page_title': 'Are you Advisor or Seeker?'
     }
-    return render(request, 'home/advisor-or-seeker.html', context)
+
+    # if the logged in user has a user profile already, complete the advisor or
+    # the seeker signup process
+
+    if UserProfile.objects.filter(user=request.user).exists():
+
+        user = UserProfile.objects.get(user=request.user)
+
+        if user.user_type == 0:
+            return redirect('advisor_signup')
+
+        elif user.user_type == 1:
+            return redirect('seeker_signup')
+
+    # if the logged in user doesn't have a profile, render the
+    # /advisor-or-seeker/ template to choose a user type.
+
+    else:
+        return render(request, 'home/advisor-or-seeker.html', context)
