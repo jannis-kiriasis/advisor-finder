2c03382ed4c8d249d6fa6cd9dfbc92702b6c8e39
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2023-02-15 23:00:23 +0000

    Add defensive design to advisor and seeker profile updates

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,21 @@
 const deleteButton = document.getElementById('delete');
+const updateAdvisorButton = document.getElementById('update_advisor');
+const updateSeekerButton = document.getElementById('update_seeker');
+
 
 // Event listeners for SweetAlerts defensive design
-
 if (deleteButton) {
     deleteButton.addEventListener('click', confirmDelete);
 }
+
+if (updateAdvisorButton) {
+    updateAdvisorButton.addEventListener('click', confirmUpdateAdvisor);
+}
+
+if (updateSeekerButton) {
+    updateSeekerButton.addEventListener('click', confirmUpdateSeeker);
+}
+
 
 /** Get href url of button delete.
 */
@@ -12,6 +23,7 @@ function goToDeleteUrl() {
     let href = document.getElementById('delete').getAttribute('href');
     window.location.href = `${href}`;
 }
+
 
 /** Prevent button click, fire SweetAlerts2, 
 * after defensive design redirect to /delete/{{ project.id }}
@@ -31,6 +43,68 @@ function confirmDelete(event) {
     }).then((result) => {
         if (result.isConfirmed) {
             goToDeleteUrl();
+        }
+    });
+}
+
+
+/** Get href url of button update advisor.
+*/
+function goToUpdateUrlAdvisor() {
+    let href = document.getElementById('update_advisor').getAttribute(
+        'href');
+    window.location.href = `${href}`;
+}
+
+
+/** Prevent button click, fire SweetAlerts2, 
+* after defensive design redirect.
+*/
+function confirmUpdateAdvisor(event) {
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure you want to update your profile?',
+        text: 
+        "Your profile will be sent to Advice Found for approval. You won't be able to receive new clients while your profile in under review.",
+        icon: 'warning',
+        iconColor: 'var(--tan)',
+        showCancelButton: true,
+        confirmButtonColor: 'var(--verdigris)',
+        cancelButtonColor: 'var(--fuzzy-wuzzy)',
+        confirmButtonText: 'Yes, update it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            goToUpdateUrlAdvisor();
+        }
+    });
+}
+
+
+/** Get href url of button update seeker.
+*/
+function goToUpdateUrlSeeker() {
+    let href = document.getElementById('update_seeker').getAttribute(
+        'href');
+    window.location.href = `${href}`;
+}
+
+
+/** Prevent button click, fire SweetAlerts2, 
+* after defensive design redirect.
+*/
+function confirmUpdateSeeker(event) {
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure you want to update you profile?',
+        icon: 'warning',
+        iconColor: 'var(--tan)',
+        showCancelButton: true,
+        confirmButtonColor: 'var(--verdigris)',
+        cancelButtonColor: 'var(--fuzzy-wuzzy)',
+        confirmButtonText: 'Yes, update it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            goToUpdateUrlSeeker();
         }
     });
 }
