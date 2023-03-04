914cfc29c9e9e1c26043d0f4758d9d565dbd5cd8
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2023-03-04 16:32:44 +0000

    Create confirmation emails of successfll payment

--- consultations/services.py
+++ consultations/services.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from checkout.emails import consultation_confirmed_email, payment_failed_email
-from checkout.emails import payment_succeeded_email
+# from checkout.emails import consultation_confirmed_email, payment_failed_email
+# from checkout.emails import payment_succeeded_email
 
 
 def confirm_consultation(order):
@@ -9,5 +9,5 @@ def confirm_consultation(order):
     get_consultation.status = 1
     get_consultation.save()
 
-    consultation_confirmed_email(order)
-    payment_succeeded_email(order)
+    # consultation_confirmed_email(order)
+    # payment_succeeded_email(order)
