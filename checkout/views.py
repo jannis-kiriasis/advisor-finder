a05e9f3a5b0440a37fbe87d126a1580d0f505dde
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2023-03-04 20:09:27 +0000

    Hide checkout if there are no consultations to checkout.

--- checkout/views.py
+++ checkout/views.py
@@ -38,104 +38,128 @@ def cache_checkout_data(request):
 
 @login_required
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    # Get the seeker profile
-    get_seeker_profile = get_object_or_404(
-        SeekerUserProfile,
-        user=request.user
-    )
+    hide_checkout = False
+    consultations_not_confirmed = False
 
-    # Get the seeker matches (it's 1 only)
-    get_all_matches = Match.objects.filter(seeker=get_seeker_profile)
-
-    # Get latest consultation schedule of the match if it exisits.
-    # If it doesn't exist redirect user to chat.
-    try:
-        consultation = Consultation.objects.filter(
-            match__id__in=get_all_matches,
-            status=0
+    if hide_checkout is False:
+        try:
+            consultations_not_confirmed = get_object_or_404(
+                Consultation,
+                status=0
             )
-    except Consultation.DoesNotExist:
-        message.error(request, ("Consultation doesn't exist."))
 
-        return redirect('advisor')
+            hide_checkout = True
+        except:
+            pass
 
-    total = consultation.price
+    if consultations_not_confirmed:
 
-    af_fee = total * 5 / 100
-    grand_total = total + af_fee
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-
-        order_form = OrderForm(request.POST)
-
-        if order_form.is_valid():
-
-            order_form.instance.fee = total
-            order_form.instance.af_fee = af_fee
-            order_form.instance.grand_total = grand_total
-
-            stripe_total = round(total * 100)
-            order_form.instance.stripe_total = stripe_total
-
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.save()
-
-            request.session['save_consultation'] = 'save-consultation' in request.POST
-            request.session['save_seeker'] = 'save-seeker' in request.POST
-            request.session['save_last_name'] = 'save-last-name' in request.POST
-            request.session['save_info'] = 'save-info' in request.POST
-
-            return redirect(reverse(
-                'checkout_success', args=[order.order_number]
-            ))
-
-        else:
-
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
-
-    elif request.method == 'GET':
-
-        stripe_total = round(total * 100)
-
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
+        # Get the seeker profile
+        get_seeker_profile = get_object_or_404(
+            SeekerUserProfile,
+            user=request.user
         )
 
-        data = {
-            'name': request.user.first_name,
-            'last_name': request.user.last_name,
-            'email': request.user.email,
-            'phone': request.user.profiles.phone_number,
-            'postcode': get_seeker_profile.postcode,
-            'town_or_city': get_seeker_profile.town_or_city,
-            'Address': get_seeker_profile.street_address,
-            'consultation': consultation,
-            'seeker': get_seeker_profile
+        # Get the seeker matches (it's 1 only)
+        get_all_matches = Match.objects.filter(seeker=get_seeker_profile)
+
+        # Get latest consultation schedule of the match if it exisits.
+        # If it doesn't exist redirect user to chat.
+        try:
+            consultation = Consultation.objects.filter(
+                match__id__in=get_all_matches,
+                status=0
+                )
+        except Consultation.DoesNotExist:
+            message.error(request, ("Consultation doesn't exist."))
+
+            return redirect('advisor')
+
+        total = consultation.price
+
+        af_fee = total * 5 / 100
+        grand_total = total + af_fee
+
+        if request.method == 'POST':
+
+            order_form = OrderForm(request.POST)
+
+            if order_form.is_valid():
+
+                order_form.instance.fee = total
+                order_form.instance.af_fee = af_fee
+                order_form.instance.grand_total = grand_total
+
+                stripe_total = round(total * 100)
+                order_form.instance.stripe_total = stripe_total
+
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.save()
+
+                request.session['save_consultation'] = 'save-consultation' in request.POST
+                request.session['save_seeker'] = 'save-seeker' in request.POST
+                request.session['save_last_name'] = 'save-last-name' in request.POST
+                request.session['save_info'] = 'save-info' in request.POST
+
+                return redirect(reverse(
+                    'checkout_success', args=[order.order_number]
+                ))
+
+            else:
+
+                messages.error(request, 'There was an error with your form. \
+                    Please double check your information.')
+
+        elif request.method == 'GET':
+
+            stripe_total = round(total * 100)
+
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
+
+            data = {
+                'name': request.user.first_name,
+                'last_name': request.user.last_name,
+                'email': request.user.email,
+                'phone': request.user.profiles.phone_number,
+                'postcode': get_seeker_profile.postcode,
+                'town_or_city': get_seeker_profile.town_or_city,
+                'Address': get_seeker_profile.street_address,
+                'consultation': consultation,
+                'seeker': get_seeker_profile
+            }
+
+            order_form = OrderForm(data)
+
+            if not stripe_public_key:
+                messages.warning(request, 'Stripe public key is missing. \
+                    Did you forget to set it in your environment?')
+
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+            'page_title': 'Checkout',
+            'fee': total,
+            'af_fee': af_fee,
+            'grand_total': grand_total
         }
 
-        order_form = OrderForm(data)
+    elif consultations_not_confirmed is False:
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
-
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-        'page_title': 'Checkout',
-        'fee': total,
-        'af_fee': af_fee,
-        'grand_total': grand_total
-    }
+        context = {
+            'message': 'There is no consultation to checkout.',
+            'no_consultation': consultations_not_confirmed
+        }
 
     return render(request, 'checkout/checkout.html', context)
 
