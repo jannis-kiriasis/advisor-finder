8faea6f05166f73214b1f11a9c52f9e1a0de47ba
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2023-03-14 00:37:02 +0000

    Add new database

--- advisor_finder/settings.py
+++ advisor_finder/settings.py
@@ -32,8 +32,9 @@ ALLAUTH_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
 DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['advisor-finder.herokuapp.com', 'localhost']
 
